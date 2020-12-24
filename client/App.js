d2a11f388e32b38fab0d13f038a068eb75a3a9ea
Shalini-CEG <ahana298@gmail.com>
2020-12-24 11:59:17 +0530

    Commit

--- client/App.js
+++ client/App.js
@@ -2,17 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SwitchTabs from "./navigations/SwitchTabs";
-import Post from "./screens/Post";
 
 export default function App() {
-  return <Post />;
+  return <SwitchTabs />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- client/navigations/SwitchTabs.js
+++ client/navigations/SwitchTabs.js
@@ -1,14 +1,59 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import React from "react";
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Content from "../screens/Content";
 import Post from "../screens/Post";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e91e63"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: "tomato" }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Content}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Post}
+        options={{
+          tabBarLabel: "Updates",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Post}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function SwitchTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Post} />
-      <Tab.Screen name="Settings" component={Post} />
-    </Tab.Navigator>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
