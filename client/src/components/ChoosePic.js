1b861cdd3609aebaf2c17cdfc21fac1e7dcbe705
Shalini-CEG <ahana298@gmail.com>
2021-01-06 18:38:27 +0530

    Commit

--- client/src/components/ChoosePic.js
+++ client/src/components/ChoosePic.js
@@ -8,7 +8,6 @@ export default function ProductImg(props) {
   const dummyImg =
     "https://www.lankabangla.com/wp-content/uploads/2019/08/no_image_png_935227.png";
   const [image, setImage] = useState(dummyImg);
-  // const imgInputRef = useRef(null);
 
   useEffect(() => {
     (async () => {
