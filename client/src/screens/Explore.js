c9a655aeef88f243600473ab49f06255ba634b80
arsan13 <arsan.anbu97@gmail.com>
2021-12-15 14:18:06 +0530

    Added dynamic search

--- client/src/screens/Explore.js
+++ client/src/screens/Explore.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { Searchbar } from "react-native-paper";
 import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import TopBar from "../components/TopBar";
@@ -11,32 +11,39 @@ import axios from "axios";
 export default function ExploreScreen({ navigation }) {
   const [searchQuery, setSearchQuery] = React.useState("");
   const [storesList, setStoresList] = React.useState([]);
+  const [filteredList, setFilteredList] = React.useState([]);
   const [open, setOpen] = React.useState(false);
   const [temp, setTemp] = React.useState("");
 
   const onChangeSearch = (query) => {
     setOpen(false);
-    setSearchQuery(query);
-    setStoresList([]);
+    // setSearchQuery(query);
+    // setStoresList([]);
+    let tempStores = storesList.filter(
+      (item) =>
+        item.companyName.toLowerCase().search(query.toLowerCase()) !== -1
+    );
+    setFilteredList(tempStores);
   };
 
-  const fetchStores = () => {
-    axios({
-      url: `${config.URI}/profile/stores${searchQuery}`,
-    })
-      .then((res) => {
-        console.log("Check out data");
-        const data = res.data;
-        if (data.length > 0) {
-          setStoresList(data);
-        }
-        setTemp(searchQuery);
-        setSearchQuery("");
+  useEffect(() => {
+    const fetchStores = () => {
+      axios({
+        url: `${config.URI}/profile/stores`,
       })
-      .catch(() => {
-        console.log("Error in retriving data..");
-      });
-  };
+        .then((res) => {
+          console.log("Check out data");
+          const data = res.data;
+          if (data.length > 0) {
+            setStoresList(data);
+          }
+        })
+        .catch(() => {
+          console.log("Error in retriving data..");
+        });
+    };
+    fetchStores();
+  }, []);
 
   const LeftContent = () => (
     <AntDesign name="profile" size={24} color="black" />
@@ -50,9 +57,9 @@ export default function ExploreScreen({ navigation }) {
     </TouchableOpacity>
   );
 
-  const openProfile = () => {
+  const openProfile = (value) => {
     setOpen(true);
-    setStoresList([]);
+    setTemp(value);
   };
 
   return (
@@ -62,23 +69,21 @@ export default function ExploreScreen({ navigation }) {
       </View>
       <Searchbar
         placeholder="Search"
-        onIconPress={fetchStores}
+        // onIconPress={fetchStores}
         onChangeText={onChangeSearch}
-        value={searchQuery}
+        // value={searchQuery}
         style={styles.explore}
       />
-
       {open && <Store companyName={temp} />}
-
-      {storesList && (
+      {filteredList && (
         <ScrollView>
-          {storesList.map((item) => (
-            <Card key={item._id} onPress={openProfile}>
+          {filteredList.map((item) => (
+            <Card key={item._id} onPress={() => openProfile(item.companyName)}>
               <Card.Title
                 title={item.companyName}
                 subtitle={item.slogan}
                 left={LeftContent}
-                right={RightContent}
+                // right={RightContent}
               />
             </Card>
           ))}
